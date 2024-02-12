8eea5c6cf586b00560fa12e762d57a219e988a4b
Bolat-JB <156825417+Bolat-JB@users.noreply.github.com>
2024-02-12 23:13:18 +0600

    6.py

--- Lab3/Classes/6.py
+++ Lab3/Classes/6.py
@@ -1,3 +1,17 @@
-lst = [1, 3, 21, 17, 44, 65, 5]
-new = filter(lambda k: all(k % x for x in range(2, int(k ** 0.5)+1)) and k > 2, lst)
-print(list(new))
+def primedel(n):
+    if n <= 3:
+        return False
+    if n % 2 == 0 or n % 3 == 0:
+        return True
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return True
+        i += 6
+    return False
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+without_prime = list(filter(lambda x: primedel(x), numbers))
+
+print("Remaining numbers in the list:", without_prime)
