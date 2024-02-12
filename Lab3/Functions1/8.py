7a536f870a0eac92244ca0b66dd2e07f3b3223da
Bolat-JB <156825417+Bolat-JB@users.noreply.github.com>
2024-02-13 02:04:16 +0600

    8.py

--- Lab3/Functions1/8.py
+++ Lab3/Functions1/8.py
@@ -1,16 +1,15 @@
 def spy_game(nums):
-    lista = []
-    listb = []
-    for x in range(0, len(nums)):
-        if nums[x] == 0:
-            lista.append(x)
-        if nums[x] == 7:
-            listb.append(x)
-    if len(lista) > 1 and len(listb) > 0:
-        if listb[0] > lista[0] and listb[0] > lista[1]:
-            return True
+    for i in range(len(nums)):
+        if nums[i]==0:
+            i+=1
+            for j in range(i, len(nums)):
+                if nums[j]==0:
+                    j+=1
+                    for k in range(j, len(nums)):
+                        if nums[k]==7:
+                            return True
     return False
 
 print(spy_game([1,2,4,0,0,7,5]))
-print(spy_game([1,0,2,4,0,5,7])) 
+print(spy_game([1,0,2,4,0,5,7]))
 print(spy_game([1,7,2,0,4,5,0]))
